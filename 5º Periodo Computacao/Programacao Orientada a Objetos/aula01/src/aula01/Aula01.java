56a6c664229833416b774b2a226faedad9b36f5e
WhoisBsa <mbscbsjbs@gmail.com>
2019-08-09 15:34:59 -0300

    POO

---
+++ 5º Periodo Computacao/Programacao Orientada a Objetos/aula01/src/aula01/Aula01.java
@@ -0,0 +1,37 @@
+package aula01;
+
+import Classes.Cliente;
+import java.util.Scanner;
+
+public class Aula01 {
+
+    public static void main(String[] args) {
+        Cliente c1 = new Cliente();
+
+        c1.nome = "Matheus";
+        c1.idade = 19;
+        c1.cpf = "11122233344";
+
+        System.out.println(c1.nome + " tem " + c1.idade + 
+        	" anos de idade de cpf: " + c1.cpf);
+
+
+        Cliente c2 = new Cliente();
+
+        Scanner s = new Scanner(System.in);
+
+    	System.out.println("Bem vindo ao cadastro de clientes");
+    	System.out.println("Nome: ");
+    	c2.nome = s.nextLine();
+
+    	System.out.println("CPF: ");
+    	c2.cpf = s.nextLine();
+
+    	System.out.println("Idade: ");
+    	c2.idade = Integer.parseInt(s.nextLine());
+
+    	System.out.println(c2.nome + " tem " + c2.idade + 
+        	" anos de idade de cpf: " + c2.cpf);
+    }
+    
+}
