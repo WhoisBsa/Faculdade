f14c98c4eabad750ea33872024bce1c09daa8db6
WhoisBsa <mbscbsjbs@gmail.com>
2019-10-02 10:50:07 -0300

    Alterar método remove

--- 5º Periodo Computacao/Programacao Orientada a Objetos/sistemaBancário2/src/Classes/Banco.java
+++ 5º Periodo Computacao/Programacao Orientada a Objetos/sistemaBancário2/src/Classes/Banco.java
@@ -48,20 +48,23 @@ public class Banco {
         return null;
     }
     
-    public ContaCorrente remove(int numConta) {
-        for(int i = 0; i < posPoupanca; i++) {
+    public ContaCorrente remove(int numConta, ContaCorrente cC) {
+        for(int i = 0; i < posCorrente; i++) {
             if(this.contaCorrente[i].getNumConta() == numConta) {
                 contaCorrente[i] = null;
                 return null;
             }
         }
-        
-        /*for(int i = 0; i < posPoupanca; i++) {
+        return contaCorrente[0];   
+    }
+    
+    public ContaPoupanca remove(int numConta, ContaPoupanca cP) {
+        for(int i = 0; i < posPoupanca; i++) {
             if(this.contaPoupanca[i].getNumConta() == numConta) {
                 contaPoupanca[i] = null;
                 return this.contaPoupanca[i];
             }
-        }*/
-        return contaCorrente[0];   
+        }
+        return contaPoupanca[0];   
     }
 }
