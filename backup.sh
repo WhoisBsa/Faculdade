a10590ffe86652a9bf2d5602069f03c7e1fa8b89
WhoisBsa <mbscbsjbs@gmail.com>
2019-11-20 16:45:29 -0300

    Arquivo de instalação de programas

---
+++ backup.sh
@@ -0,0 +1,23 @@
+echo "Instalando arquivos.
+Isso pode demorar um pouco..."
+
+# Arquivos a serem instalados
+vetor=(google-chrome-stable 
+        code 
+        spotify-client 
+        telegram-desktop 
+        slack-desktop 
+        peek 
+        sublime-text 
+        whatsapp-desktop)
+
+for i in "${vetor[@]}"
+do
+    printf "\n\nInstalando $i\n\n"
+    if ! (apt-get install $i -y)
+    then
+        echo -e "\n\nNão foi possível instalar o pacote $1\n\n"
+    else
+        echo -e "\n\nInstalação de $i finalizada\n\n"
+    fi 
+done
